fe3dc5c85c4dabc002be574ac47f75b5e0d63f25
braydensoneill <braydensoneill@gmail.com>
2025-02-06 07:35:00 +0000

    Learn React - Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div>
-      <Alert>
-        <span>Hello World!</span>
-      </Alert>
-    </div>
+    <Button color="primary" onClick={() => console.log("Clicked")}>
+      My Button
+    </Button>
   );
 }
 
