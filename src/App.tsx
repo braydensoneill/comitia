7bfe679a17e1f852c775067631535d8f8c3b7ec6
braydensoneill <braydensoneill@gmail.com>
2025-02-06 07:15:54 +0000

    Learn React - Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Clonmel", "Thurles", "Limerick"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        <span>Hello World!</span>
+      </Alert>
     </div>
   );
 }
