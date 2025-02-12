022da8787bc524c42768658aa6d048878824789d
braydensoneill <braydensoneill@gmail.com>
2025-02-12 02:53:41 +0000

    update profile / authentication functionality

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req,resizeBy,next) => {
+    try {
+        const token = req.cookies.jwt; // "jwt" name was made in lib/utils.js
+
+        if(!token) {
+            return res.status(401).json({message: "Unauthorised - No token provided"});
+        }
+
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+        if(!decoded) {
+            return res.status(401).json({message: "Unauthorised - Invalid token"});
+        }
+
+        const user = await User.findById(decoded.userId).select("password");
+
+        if(!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+
+        req.user = user;
+
+        next();
+    }
+
+    catch (error) {
+        console.log("Error in protectRoute middleware: ", error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
